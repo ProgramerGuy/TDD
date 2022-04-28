81e028f17871e11b17a360a01d8bccfe2988e201
RaulG <raul.animations@gmail.com>
2022-04-27 19:35:12 -0500

    Se agrega clase spiderman

---
+++ app/spiderman.js
@@ -0,0 +1,11 @@
+class Spiderman {
+    constructor(name, age, actorName, moviesCount, studios) {
+        this.name = name;
+        this.age = age;
+        this.actorName = actorName;
+        this.moviesCount = moviesCount;
+        this.studios = studios;
+    }
+}
+
+module.exports = Spiderman;

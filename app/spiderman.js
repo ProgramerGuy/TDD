b18d9db2d82491cbbe3c4a4672f704db5b3a44ac
RaulG <raul.animations@gmail.com>
2022-04-27 23:01:32 -0500

    Se agregan pruebas con la clase y el metodo getInfo

--- app/spiderman.js
+++ app/spiderman.js
@@ -6,6 +6,10 @@ class Spiderman {
         this.moviesCount = moviesCount;
         this.studios = studios;
     }
+
+    getInfo() {
+        return `Hey, I'm ${this.actorName} from ${this.studios} Studio`
+    }
 }
 
 module.exports = Spiderman;
